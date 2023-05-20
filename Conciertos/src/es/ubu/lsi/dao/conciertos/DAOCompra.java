aa2eec65100e8949fe07841322249598cdda8323
ecl1009 <ecl1009@alu.ubu.es>
2023-05-21 01:03:27 +0200

    Implementación DAOs

---
+++ Conciertos/src/es/ubu/lsi/dao/conciertos/DAOCompra.java
@@ -0,0 +1,139 @@
+package es.ubu.lsi.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import es.ubu.lsi.model.conciertos.*;
+import es.ubu.lsi.service.conciertos.IncidentException;
+
+/**
+ * Clase que implementa el patrón DAO para acceder a los datos de la tabla compra.
+ * 
+ ** @author Eduardo Manuel Cabeza Lopez
+ *
+ * @param <E> tipo de la entidad
+ * @param <K> tipo de la pk
+ */
+public class DAOCompra<E, K> extends JpaDAO<Compra, Integer> {
+
+	/**
+	 * Constructor de la clase DAOCompra.
+	 * 
+	 * @param em Gestor de entidades
+	 */
+	public DAOCompra(EntityManager em) {
+		super(em);
+	}
+
+	/**
+	 * Busqueda de compras por cliente a la base de datos.
+	 * 
+	 * @param idCliente PK del cliente buscado
+	 * @return Lista de compras asociadas al cliente
+	 * @throws IncidentException 
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Compra> findByCliente(int idCliente) throws IncidentException {
+		Query query = getEntityManager().createQuery("select c " + "from Compra c " + "where c.idcliente = :idCliente");
+		query.setParameter("idCliente", idCliente);
+		List<Compra> compras = query.getResultList();
+		if (!compras.isEmpty()) {
+			return compras;
+		}
+		return null;
+
+	}
+
+	/**
+	 * Devuelve el siguiente idcompra a usar realizando una busqueda en la base de datos del campo idcompra más alto e incrementandolo en una unidad. 
+	 * 
+	 * 
+	 * @return Siguiente idcompra
+	 */
+	public int findNextId() {
+		Compra compra = getEntityManager().createQuery("select c" + " from Compra c " + "where c.idcompra = "
+				+ "(select max(c2.idcompra) " + "from Compra c2)", Compra.class).getSingleResult();
+		int currentId = compra.getIdcompra();
+
+		return currentId + 1;
+	}
+
+	/**
+	 * Inserción de una fila en la tabla compra en la base de datos
+	 * 
+	 * @param idCompra PK de la compra
+	 * @param cliente Cliente asociado a la compra
+	 * @param concierto Concierto asociado a la compra.
+	 * @param tickets Tickets comprados en la transacción
+	 * @param compra Entidad compra a insertar en la base de datos
+	 */
+	public void insertCompra(int idCompra, Cliente cliente, Concierto concierto, int tickets, Compra compra) {
+
+		compra.setIdcompra(idCompra);
+		compra.setCliente(cliente);
+		compra.setConcierto(concierto);
+		compra.setNTickets(tickets);
+
+	}
+
+	/**
+	 * Busqueda de todas las compras para conciertos de un mismo grupo en la base de datos
+	 * 
+	 * @param grupo Grupo para el que se buscan las compras
+	 * @return Lista de compras
+	 */
+	public List<Compra> findAllByGroup(int grupo) {
+		List<Compra> compras = getEntityManager()
+				.createQuery("select c " + "from Compra c " + "where c.concierto.grupo.idgrupo = :grupo", Compra.class)
+				.setParameter("grupo", grupo).getResultList();
+		return compras;
+	}
+
+	
+	
+	/**
+	 * Redefinición del método findAll que solicita una busqueda de todas las compras en la base de datos.
+	 * 
+	 * @return Lista de compras
+	 */
+	@Override
+	public List<Compra> findAll() {
+		List<Compra> compras = getEntityManager().createQuery("select c " + "from Compra c", Compra.class)
+				.getResultList();
+
+		return compras;
+	}
+
+	/**
+	 * Busqueda de compras asociadas a un mismo concierto en la base de datos.
+	 * 
+	 * @param concierto Concierto para el que se buscan las compras
+	 * @return Lista de compras
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Compra> findByConcierto(Concierto concierto) {
+
+		Query query = getEntityManager()
+				.createQuery("select c " + "from Compra c " + "where idConcierto = :idConcierto");
+		query.setParameter("idConcierto", concierto);
+		List<Compra> compras = query.getResultList();
+		if (!compras.isEmpty()) {
+			return compras;
+		}
+		return null;
+
+	}
+	
+	/**
+	 * Elimina todas las compras asociadas a un concierto.
+	 * Realiza la operación contra la base de datos. Hay que tener precaución en su uso.
+	 * 
+	 * @param concierto Concierto del que se quieren eliminar todas las compras.
+	 */
+	public void removeByConcierto(Concierto concierto) {		
+		getEntityManager().createQuery("delete from Compra c where c.concierto.idconcierto = ?1").setParameter(1, concierto.getIdconcierto()).executeUpdate();
+	}
+
+}
---
+++ Conciertos/src/es/ubu/lsi/dao/conciertos/DAOConcierto.java
@@ -0,0 +1,89 @@
+package es.ubu.lsi.dao;
+
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TemporalType;
+import es.ubu.lsi.model.conciertos.Concierto;
+import es.ubu.lsi.service.conciertos.IncidentException;
+
+/**
+ * Clase que implementa el patrón DAO para acceder a los datos de la tabla concierto.
+ * 
+ * @author Eduardo Manuel Cabeza Lopez
+ *
+ * @param <E> tipo de la entidad
+ * @param <K> tipo de la pk
+ */
+public class DAOConcierto<E, K> extends JpaDAO<Concierto, Integer> {
+
+	
+	/**
+	 * Constructor de la clase DAOConcierto
+	 * 
+	 * @param em Gestor de entidades
+	 */
+	public DAOConcierto(EntityManager em) {
+		super(em);		
+	}
+
+	/**
+	 * Busqueda de conciertos para un grupo y una fecha determinados en la base de datos.
+	 * 
+	 * @param fecha Fecha del concierto
+	 * @param idGrupo PK del grupo
+	 * @return Lista de conciertos
+	 * @throws IncidentException
+	 */
+	// @SuppressWarnings("unchecked")
+	public List<Concierto> findByFechaAndGrupo(Date fecha, int idGrupo) throws IncidentException {
+
+		List<Concierto> concierto = getEntityManager()
+				.createQuery("select c "
+						+ "from Concierto c "
+						+ "where c.fecha = ?1 "
+						+ "and c.grupo.idgrupo = ?2", Concierto.class)
+				.setParameter(1, fecha, TemporalType.TIMESTAMP).setParameter(2, idGrupo).getResultList();
+		return concierto;
+	}
+			
+	/**
+	 * Busqueda de conciertos asociados a un grupo en la base de datos.
+	 * 
+	 * @param grupo Grupo para el que se buscan los conciertos
+	 * @return Lista de conciertos
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Concierto> findByGrupo(int grupo) {
+		return getEntityManager().createQuery("select c "
+				+ "from Concierto c "
+				+ "where c.grupo.idgrupo = :idGrupo")
+		.setParameter("idGrupo", grupo).getResultList();		
+	}
+
+	/**
+	 *Actualiza los tickets de un concierto.
+	 * 
+	 * @param tickets Nueva cantidad de tickets
+	 * @param concierto Concierto al que se le modifica el número de tickets
+	 */
+	public void updateTickets(int tickets, Concierto concierto) {
+		concierto.setTickets(tickets);
+	}
+
+	/**
+	 * Redefinición del método findAll que realiza una busqueda de todos los conciertos en la base de datos
+	 * 
+	 * @return Lista de conciertos
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<Concierto> findAll() {
+		Query query = getEntityManager().createQuery("select c "
+				+ "from concierto c");
+		List<Concierto> conciertos = query.getResultList();
+		return conciertos;
+	}
+}
